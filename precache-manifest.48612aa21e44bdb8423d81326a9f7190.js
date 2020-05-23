62dff07a44f79d45b1feb7d7493547280971dba1
Karan Chauhan <ckaran40@gmail.com>
2020-05-22 20:56:42 -0400

    React code

---
+++ precache-manifest.48612aa21e44bdb8423d81326a9f7190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "500608396953ca6797b3c302eb41fe92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b10eda15df865271a98",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "6486fa4257ee4e6437a1",
+    "url": "/static/css/main.f80ac13c.chunk.css"
+  },
+  {
+    "revision": "3b10eda15df865271a98",
+    "url": "/static/js/2.0d8862e0.chunk.js"
+  },
+  {
+    "revision": "6486fa4257ee4e6437a1",
+    "url": "/static/js/main.4539d952.chunk.js"
+  },
+  {
+    "revision": "6d60b4ae05f839f97de9",
+    "url": "/static/js/runtime-main.b9740393.js"
+  }
+]);
